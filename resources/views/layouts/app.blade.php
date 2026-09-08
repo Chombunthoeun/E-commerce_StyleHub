4532af828a6bf57bdb104cceb378c91051a993be
Bunthoeun <konmak682@gmail.com>
2026-09-08 12:42:59 +0700

    update code ci cd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
             </div>
 
             <div class="footer-col footer-col--center">
-                <h4>Contact</h4>
+                <h4>Contact me</h4>
                 <a href="https://t.me/Chombunthoeun" target="_blank" rel="noopener" class="btn btn-primary btn-sm btn-telegram" aria-label="Message us on Telegram" title="Message us on Telegram"><svg viewBox="0 0 240 240" width="18" height="18" fill="currentColor" aria-hidden="true"><path d="M120 0C53.7 0 0 53.7 0 120s53.7 120 120 120 120-53.7 120-120S186.3 0 120 0Zm56.2 79.6-19.6 92.5c-1.5 6.6-5.4 8.2-10.9 5.1l-30.1-22.2-14.5 14c-1.6 1.6-2.9 2.9-6 2.9l2.1-30.6 55.8-50.4c2.4-2.2-.5-3.4-3.8-1.2l-69 43.5-29.7-9.3c-6.5-2-6.6-6.5 1.3-9.6l116.1-44.8c5.4-2 10.1 1.3 8.3 9.1Z"/></svg></a>
             </div>
 
